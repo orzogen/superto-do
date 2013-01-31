830be07f3966157993b8a3025ec69081400df6a0
Dan Ruswick <orzogen@gmail.com>
2013-01-31 11:42:40 -0600

    added priority items

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ function uuid() {
 }
 			
 var Item = Backbone.Model.extend({
-	defaults: {entry: "", id: 0}
+	defaults: {entry: "", id: 0, important: false}
 });
         	
 var Items = Backbone.Collection.extend({
@@ -24,25 +24,34 @@ populateCollectionFromLocalStorage(items);
 // THIS IS TRIGGERED WHEN ADDING AN ITEM    	
 $("#add").keydown(function(key){
     var code = key.which; // Get code of pressed key.
-    if(code == 13) key.preventDefault();
     
-    if(code==13||code==188||code==186){ // If the code indicates a press of enter key, save the item.
+    if(code==13){ // If the code indicates a press of enter key, save the item.
+        key.preventDefault();
     	var entry = $('#add').val(); // Get text of the item from the input field.
     	if (entry!=""){
-        	items.add({entry: entry, id: uuid()}); // Generate item and add to list.
-        	draw(items); // Draw items.
-        	reMapItemDeleteClickHandlers();
+    	    var important = (entry.match(/.+\*/)) ? true : false;
+    	    if(important){entry = entry.slice(0, -1);}
+        	items.add({entry: entry, id: uuid(), important: important}); // Generate item and add to list.
         	saveItemsInCollectionToLocalStorage(items);
+        	draw(items); // Draw items.
         }
     }
 });
 
 // THIS IS CALLED TO REDRAW THE VIEW
 function draw(collection){ 
-	$("#list").html(""); // Reset the displayed items.
+	$("#important").html(""); 
+	$("#placehold-container").html("");
+	$("#normal").html(""); // Reset the displayed items.
 	
     collection.each(function(item){ // Add a row for each item.
-      	$("#list").prepend('<div class="row" ' + 'data-id="' + item.get("id") + '">' + item.get("entry") + '</row>');
+        if(item.get("important")){
+            $("#important").prepend('<div class="row" ' + 'data-id="' + item.get("id") + '">' + item.get("entry") + '</row>');
+            console.log("imp");
+        }
+        else{
+      	    $("#normal").prepend('<div class="row" ' + 'data-id="' + item.get("id") + '">' + item.get("entry") + '</row>');
+      	}
     }); 
     
 	$("#add").val(""); // Clear the input field.
@@ -56,28 +65,30 @@ function reMapItemDeleteClickHandlers(){
 	$(".row").click(function(){
 		var item = items.get($(this).data("id"));
 		items.remove(item);
-		draw(items);
 		saveItemsInCollectionToLocalStorage(items);
+		$("#add").focus();
+		draw(items);
 	});
 }
 
 function insertPlaceholderIfEmptyList(collection){ // Called after drawing view
 	if (collection.length == 0){
-		$("#list").prepend('<div class="row placeholder">Add Something ↑</div>');
+		$("#placehold-container").prepend('<div class="row placeholder">Add Something above, and append "*" for important items.</div>');
 	}
 }
 
 function saveItemsInCollectionToLocalStorage(collection){
 	localStorage.clear(); // Clear localStorage before rewriting to it.
 	collection.each(function(item){
-		localStorage.setItem(item.get("id"), item.get("entry"));
+	    var itemData = JSON.stringify({entry: item.get("entry"), important: item.get("important")});
+		localStorage.setItem(item.get("id"), itemData);
 	});
 }
 
 function populateCollectionFromLocalStorage(collection){
 	for (var i = 0; i < localStorage.length; i++){
-    	var item = localStorage.getItem(localStorage.key(i));
-    	items.add({entry: item, id: localStorage.key(i)});
+    	var item = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    	items.add({entry: item.entry, id: localStorage.key(i), important: item.important});
 	}
 	draw(items);
 }
